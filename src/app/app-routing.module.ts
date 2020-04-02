793563850506a3f0a453e6e59b55747472f2264c
Rory Kaufmann <rorykaufmann@gmail.com>
2020-04-02 16:16:42 -0500

    removed login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeViewComponent } from './home-view/home-view.component';
-import { LoginComponent } from './login/login.component';
 import { CallbackComponent } from './callback/callback.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeViewComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'callback', component: CallbackComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
